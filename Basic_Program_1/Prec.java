fab458ad0f21a28f3fb28454f4dc3bef9e324983
priyeranjan204-jpg <priyeranjan204@gmail.com>
2026-01-19 10:12:18 +0530

    [Priyeranjan] remove file from mainbranch

--- Basic_Program_1/Prec.java
+++
@@ -1,8 +0,0 @@
-public class Prec {
-	public static void main(String[] args){
-		int l = 10;
-		int b = 20;
-		int p = 2*(l + b);
-		System.out.println("Perimeter of Rectangle: " + p);
-	}
-}
--- Basic_Program_1/Vcyl.java
+++
@@ -1,9 +0,0 @@
-public class Vcly {
-	public static void main(String[] args) {
-		double pie =  3.14;
-		int r = 5;
-		int h = 10;
-		double V = pie*r*r*h;
-		System.out.println("Volume of cylinder is: " + V);
-	}
-}
